e7586e22659d846af5204f43423ec2dd8e5822a0
frank <frank@frank-N56VB>
2014-11-13 10:59:56 +0100

    header bijgewerkt en post in values

--- public/registreer.php
+++ public/registreer.php
@@ -7,7 +7,7 @@
 	session_start();
 ?>
 
-<h2>Maak een nieuw account aan</h2>
+<h1>Maak een nieuw account aan</h1>
 
 <?php
 	if(isset($_SESSION['errors']) || isset($_SESSION['melding'])) {
@@ -21,28 +21,28 @@
 <form action = "registreer_processing.php" method = "post">
 	<table>
 		<tr>
-			<td>E-mail adres: </td>	<td><input type = "text" name = "email"></td><td>*</td>
+			<td>E-mail adres: </td>	<td><input type = "text" name = "email" value="<?php $_POST['email']?>"></td><td>*</td>
 		</tr>
 		<tr>
 			<td>Wachtwoord: </td>	<td><input type = "password" name = "wachtwoord"></td><td>*</td>
 		</tr>
 		<tr>
-			<td>Voornaam: </td>	<td><input type = "text" maxlength="32" name = "voornaam"></td><td>*</td>
+			<td>Voornaam: </td>	<td><input type = "text" maxlength="32" name = "voornaam" value="<?php $_POST['email']?>"></td><td>*</td>
 		</tr>
 		<tr>
-			<td>Achternaam: </td>	<td><input type = "text" maxlength="32" name = "achternaam"></td><td>*</td>
+			<td>Achternaam: </td>	<td><input type = "text" maxlength="32" name = "achternaam" value="<?php $_POST['email']?>"></td><td>*</td>
 		</tr>
 		<tr>
-			<td>Adres: </td>	<td><input type = "text" name = "adres"></td><td>*</td>
+			<td>Adres: </td>	<td><input type = "text" name = "adres" value="<?php $_POST['email']?>"></td><td>*</td>
 		</tr>
 		<tr>
-			<td>Postcode: </td>	<td><input type = "text" maxlength="7" name = "postcode"></td><td>* Een postcode moet ingevoerd volgens deze volgorde: 1234 AB</td>
+			<td>Postcode: </td>	<td><input type = "text" maxlength="7" name = "postcode" value="<?php $_POST['email']?>"></td><td>* Een postcode moet ingevoerd volgens deze volgorde: 1234 AB</td>
 		</tr>
 		<tr>
 			<td>Plaats: </td>	<td><input type = "text" name = "plaats" value="Groningen" readonly="readonly" ></td><td>*</td>
 		</tr>
 		<tr>
-			<td>Telefoonnumer: </td>	<td><input type = "text" maxlength="10" name = "telefoonnummer"></td><td>*</td>
+			<td>Telefoonnumer: </td>	<td><input type = "text" maxlength="10" name = "telefoonnummer" value="<?php $_POST['email']?>"></td><td>*</td>
 		</tr>
 		<tr>
 			<td><input type = "submit" name = "registreer" value = "Registreer"</td><td></td>
