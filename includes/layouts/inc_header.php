68b22a748bf200b77ff0c144d73c594977161348
Braindamage1989 <R.Scholten@Live.nl>
2014-11-13 22:54:28 +0100

    title derppp

--- includes/layouts/inc_header.php
+++ includes/layouts/inc_header.php
@@ -2,6 +2,6 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="stylesheets/style_org.css" />
-		<title>Beat IT or Eat IT</title>
+		<title>Eat IT or Beat IT!</title>
 	</head>
 	<body>
