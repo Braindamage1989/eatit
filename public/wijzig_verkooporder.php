05b370eee26a261eb9e38727f813dc362fce70bd
Braindamage1989 <R.Scholten@Live.nl>
2014-11-13 12:17:59 +0100

    Whitespace

--- public/wijzig_verkooporder.php
+++ public/wijzig_verkooporder.php
@@ -12,6 +12,7 @@
 	if(!isset($_POST['wijzig_status'])) {
 		redirect_to("verkooporder.php");
 	}
+        
 	$verkoopordernr = $_POST['ordernr'];
 
 	$query = "SELECT * FROM `order`  WHERE ordernr = {$verkoopordernr};";
